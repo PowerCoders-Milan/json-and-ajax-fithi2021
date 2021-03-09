e953ba645df0775ff81ca61153c96ba7245c4874
fithawi ghide <lgfetah@gmail.com>
2021-03-09 12:16:34 +0100

    JSON format, Js obj,values

---
+++ ex1/exercise.js
@@ -0,0 +1,21 @@
+
+var object = {
+    id: 1,
+    title: "this is a note",
+    content: "This is the amazing content of the note!",
+    body:"This is a body of the content"
+  }
+  //changing the object to JSON Format.
+  console.log("This is JSON format");
+  const myJsonFile = JSON.stringify(object);
+  console.log(myJsonFile);
+  //changing the  JSON file to Obj.
+  console.log("This is an obj JS")
+  const myObj = JSON.parse(myJsonFile);
+  console.log(myObj);
+  //Using the forin loop 
+  for (const values in myObj) {
+      console.log(myObj[values]);
+  }
+
+  
