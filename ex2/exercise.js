b6c51982c1bd1445912b048d226fa3d95ebad1f7
fithawi ghide <lgfetah@gmail.com>
2021-03-11 00:04:39 +0100

    Print all keys and values of the Json File

--- ex2/exercise.js
+++ ex2/exercise.js
@@ -1,4 +1,20 @@
-fetch("https://jsonplaceholder.typicode.com/todos/1")
-  .then(response => response.text())
-  .then(data => document.body.innerHTML = data);
+fetch("https://jsonplaceholder.typicode.com/todos/3")
+  .then(response => response.json())
+  .then(data =>{
+   var keys = `Keys:${Object.keys(data)}`
+    document.getElementById("keys").innerHTML = keys
+    document.getElementById("values").innerHTML = `Values:${Object.values(data)}`
+  })
 
+
+//   var pro = new Promise((reslove)=>{
+//     if(data.id == 1){
+//       reslove("This is resolved")
+//     }
+
+
+// }).then(mess =>{
+//   document.getElementById("1").innerHTML = mess;
+// }).catch(mess=>{
+//   document.getElementById("1").innerHTML = mess
+// })
